a8de99fa1f4c838914fec81e8a41a767b5a3c3a7
anaswara211 <anaswaraanil874@gmail.com>
2024-11-15 15:42:09 +0530

    emp-management

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div>
       <nav className="navbar p-4 navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand fw-bolder fs-5 text-white " to="/">WORK FORCE MANAGEMENT APP</Link>
+          <Link className="navbar-brand fw-bolder fs-5 text-white " to="/">EMPLOYEE MANAGEMENT APP</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
